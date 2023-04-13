9478a22c0f14a63c9263b9d0438749a20b25227a
hira22 <m.hiraoka22@gmail.com>
2023-04-13 16:56:56 +0900

    first commit

---
+++ EventPhotoShareApp/EventPhotoShareAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventPhotoShareAppApp.swift
+//  EventPhotoShareApp
+//
+//  Created by hiraoka on 2021/10/25.
+//
+
+import SwiftUI
+
+@main
+struct EventPhotoShareAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
